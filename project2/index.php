b74b21b4a9b307574a72d9608f19b522f00efdac
kevin <delrosariokevinm@gmail.com>
2014-06-18 16:29:24 +0800

    fix error for push

--- project2/index.php
+++ project2/index.php
@@ -1,11 +1,9 @@
 <html><head><title>new project 2</title></head>
 <body>
 <p>second test git project</p>
-<<<<<<< HEAD
 <p>3rd text ,,,,, </p>
-=======
 <p> added more text</p>
->>>>>>> 895738e857aafe95068a6f3b20fce3600eb551c7
+
 </body>
 
 
